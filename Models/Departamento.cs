72c33f0b8218e48e86a1cdd165246ddc0ea2da0f
Christopher Hugo de Alvarenga Melo <christopher.hugo.a.m@gmail.com>
2023-10-03 22:04:33 -0300

    Criação das Models e Conexão com Banco de Dados

---
+++ Models/Departamento.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tickest.Models
+{
+    public class Departamento
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Nome_Departamento { get; set; }
+
+        public ICollection<Area> Areas { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Tickest.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<Contexto>(b=>b.UseSqlServer(builder.Configuration.
+    GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
