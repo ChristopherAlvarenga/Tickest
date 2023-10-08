84f42bd2e10f88f4c69a0b0090c3d7cf4e531111
Christopher Hugo de Alvarenga Melo <christopher.hugo.a.m@gmail.com>
2023-10-08 14:08:22 -0300

    Exclusão Projeto Antigo

--- Models/ViewModels/VMUsuario.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Tickest.Models.ViewModels
-{
-    public class VMUsuario
-    {
-        public string Email { get; set; }
-        public string Senha { get; set; }
-        public bool Permanecer_Logado { get; set; }
-    }
-}
